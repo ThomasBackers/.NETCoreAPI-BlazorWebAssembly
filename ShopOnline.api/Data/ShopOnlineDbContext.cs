cb7d428da354f7de1a6776b5151105d18f5d81bd
Thomas Backers <thomasbackers@gmail.com>
2022-09-06 21:48:05 +0200

    checkpoint

---
+++ ShopOnline.api/Data/ShopOnlineDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShopOnline.api.Data
+{
+    public class ShopOnlineDbContext : DbContext
+    {
+        public ShopOnlineDbContext(DbContextOptions<ShopOnlineDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
